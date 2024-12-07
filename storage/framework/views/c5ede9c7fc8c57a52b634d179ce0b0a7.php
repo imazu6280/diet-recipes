db920691736006b6d31487ad4efc1d2027f4df46
imazu6280 <imazu.shootingstar@icloud.com>
2024-12-07 19:44:06 +0900

    環境構築01

---
+++ storage/framework/views/c5ede9c7fc8c57a52b634d179ce0b0a7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>sample-api</title>
+
+        
+        <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js', 'resources/ts/index.tsx']); ?>
+
+    </head>
+
+    <body class="antialiased">
+        <div id="app"></div>
+    </body>
+</html>
+<?php /**PATH /var/www/html/resources/views/index.blade.php ENDPATH**/ ?>
