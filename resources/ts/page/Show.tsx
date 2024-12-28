ea393e899d357cfabdffe25edd05aa475756c321
imazu6280 <imazu.shootingstar@icloud.com>
2024-12-28 15:15:34 +0900

    showページのコミット

--- resources/ts/page/Show.tsx
+++ resources/ts/page/Show.tsx
@@ -1,19 +1,41 @@
 import { Button } from "../component/Button"
+import { Card } from "../component/Card"
+import { GetCardResponse } from "../type/card"
+
+const card: GetCardResponse = [
+    {
+        id: 0,
+        image: "https://placehold.jp/350x240.png",
+        detail: "ダミーテキスト",
+    },
+    {
+        id: 1,
+        image: "https://placehold.jp/350x240.png",
+        detail: "ダミーテキスト",
+    },
+    {
+        id: 2,
+        image: "https://placehold.jp/350x240.png",
+        detail: "ダミーテキスト",
+    },
+]
 
 export const Show = () => {
     return (
         <div className="flex flex-col gap-y-6 w-inner mx-auto">
-            <div className="grid grid-cols-show-column gap-x-6">
+            <div className="grid grid-cols-show-column gap-x-6 sm:block">
                 <div className="h-96 bg-gray"></div>
-                <div className="flex flex-col justify-between">
-                    <div className="flex flex-col gap-y-10">
+                <div className="flex flex-col justify-between sm:gap-y-6">
+                    <div className="flex flex-col gap-y-10 sm:gap-y-4">
                         <div className="flex flex-col gap-y-4">
                             <h2 className="text-34px font-bold">ダミーテキスト</h2>
                             <p>
                                 <span className="text-lg font-semibold">OO</span>カロリー
                             </p>
                         </div>
-                        <p>ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                        <p className="sm:p-2 sm:text-sm sm:bg-gray sm:rounded-lg">
+                            ダミーテキストダミーテキストダミーテキストダミーテキスト
+                        </p>
                     </div>
                     <div className="flex gap-x-4">
                         <Button isIcon={true} text="レシピを保存" />
@@ -21,7 +43,7 @@ export const Show = () => {
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-show-column gap-x-6">
+            <div className="grid grid-cols-show-column gap-x-6 sm:block">
                 <div className="flex flex-col gap-y-4">
                     <h3 className="text-2xl font-bold">材料</h3>
                     <p>1枚分</p>
@@ -36,21 +58,10 @@ export const Show = () => {
                 </div>
                 <div className="flex flex-col gap-y-4">
                     <h3 className="text-2xl font-bold">作り方</h3>
-                    <ul className="grid grid-cols-4 gap-x-4 gap-y-10">
-                        <li className="flex flex-col gap-y-2">
-                            <span className="w-6 h-6 text-center text-white bg-black rounded-full">
-                                1
-                            </span>
-                            <figure
-                                className="w-full py-72px rounded-lg bg-gray
-                            "
-                            >
-                                <img src="" alt="" />
-                            </figure>
-                            <figcaption>
-                                ダミーテキストダミーテキストダミーテキストダミーテキスト
-                            </figcaption>
-                        </li>
+                    <ul className="grid grid-cols-4 gap-x-4 gap-y-10 sm:grid-cols-1">
+                        {card.map((item) => (
+                            <Card key={item.id} image={item.image} detail={item.detail} />
+                        ))}
                     </ul>
                 </div>
             </div>
